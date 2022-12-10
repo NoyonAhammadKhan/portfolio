5d4d50877e5c227ea5531c9a148d7b589061774c
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-12-10 17:51:57 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Shared/Header/Header';
+
+function App() {
+  return (
+    <div className="w-10/12 mx-auto bg bg-sky-800">
+     <Header></Header>
+    </div>
+  );
+}
+
+export default App;
